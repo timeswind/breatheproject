983a125e1d0d2e3ced4237e2ad7e1d7b42eeab3a
Mingtian Yang <mky5089@psu.edu>
2019-10-01 09:21:00 -0400

    update requirement.txt

--- start.py
+++ start.py
@@ -2,12 +2,10 @@ import numpy as np
 import pandas as pd
 
 from ProjectBreathe import ProjectBreathe
-import smell_report_cleanup
 
-smellReportLink = 'data/smell_reports.csv'
+if __name__ == "__main__":
+    smellReportLink = 'data/smell_reports.csv'
 
-pb = ProjectBreathe(smellReportLink)
+    pb = ProjectBreathe(smellReportLink)
 
-print(pb.smellReport.zipCodes)
-# print(analyse.getAllZipCodes(smellReport))
-# analyse.groupByZipCode(smellReport)
+    print(pb.smellReport.zipCodes) 
