efab83e7ccfc1b6b4094d9854b5b0146bd7eec13
Mingtian Yang <mky5089@psu.edu>
2019-10-01 08:25:33 -0400

    add one hot encoding

--- smell_report_cleanup.py
+++ smell_report_cleanup.py
@@ -2,6 +2,21 @@ import pandas as pd
 
 def cleanup(df: pd.read_csv):
     df["date"] = pd.to_datetime(df['epoch time'],unit='s')
+    df['year'] = pd.DatetimeIndex(df['date']).year
+    df['month'] = pd.DatetimeIndex(df['date']).month
+    df['day'] = pd.DatetimeIndex(df['date']).day
+
+    dateHoursToDaySlotMapping = map(get_part_of_day, pd.DatetimeIndex(df['date']).hour)
+    dateHoursToDaySlotColumn = list(dateHoursToDaySlotMapping)
+
+    df['dayslot'] = dateHoursToDaySlotColumn
+
+    daySlotOneHot = pd.get_dummies(df['dayslot'])
+
+    df = df.join(daySlotOneHot)
+    #drop the slot after finish one hot encoding
+    df.drop(['dayslot'], axis=1, inplace=True)
+
     #drop those text fileds since current it is hard to analysis texts
     df.drop(['date & time',
     'epoch time',
@@ -18,3 +33,14 @@ def cleanup(df: pd.read_csv):
     # print(df.dtypes)
     # for col in df.columns: 
     #     print(col) 
+
+def get_part_of_day(hour):
+    return (
+    "isMorning" if 5 <= hour <= 11
+    else
+    "isAfternoon" if 12 <= hour <= 17
+    else
+    "isEvening" if 18 <= hour <= 22
+    else
+    "isNight"
+    )
