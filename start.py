8a2b112c26369b18a622d03e90f8e3299c624830
Mingtian Yang <mky5089@psu.edu>
2019-10-01 09:39:14 -0400

    update

--- start.py
+++ start.py
@@ -1,11 +1,57 @@
 import numpy as np
 import pandas as pd
+from os import system, name 
+
+from bullet import VerticalPrompt, Numbers, Bullet, Check, YesNo, Input # and etc...
 
 from ProjectBreathe import ProjectBreathe
 
+pb = None
+
+def choose_program():
+    executionChoices = ["CLI", "Graph Dashboard"]
+    cli = VerticalPrompt(
+    [
+        Bullet("Choose program ",
+              choices = executionChoices),
+    ],
+    spacing = 1
+    )
+    result = cli.launch()
+    executionChoice = result[0][1]
+
+    if (executionChoice == executionChoices[0]):
+        clear()
+        cli_program()
+    elif (executionChoice == executionChoices[1]):
+        print("Feature not complete")
+        choose_program()
+
+
+def cli_program():
+    executionChoices = ["Show all smell report zipcodes", "exit"]
+    cli = VerticalPrompt(
+    [
+        YesNo("Start the program "),
+        Bullet("What data? ",
+              choices = executionChoices),
+    ],
+    spacing = 1
+    )
+    result = cli.launch()
+    executionChoice = result[1][1]
+
+    if (executionChoice == executionChoices[0]):
+        print(pb.smellReport.zipCodes) 
+
+def clear(): 
+    if name == 'nt': 
+        _ = system('cls') 
+    else: 
+        _ = system('clear')
+
 if __name__ == "__main__":
     smellReportLink = 'data/smell_reports.csv'
-
     pb = ProjectBreathe(smellReportLink)
 
-    print(pb.smellReport.zipCodes) 
+    choose_program()
