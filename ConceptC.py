d6cfa814bb00f8833d13bdd4ca0174a227dd39ff
Mingtian Yang <mky5089@psu.edu>
2019-10-24 08:31:31 -0400

    update

--- ConceptC.py
+++ ConceptC.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import SmellReport as SmellReport
+
+
 # Concept C
 # Smell PGH Statistics
 # We will generate a graph to analyze the smell value over time.
 # This will give us an idea of how the air quality changed in that particular area over the years.
 # We will using Python program that will help us project all the data in a graphical scenario.
-# If we can get it done successfully,
 # the path of demonstrating the air quality to others will be user-friendly to the public.
-
 class ConceptC(object):
     smellReport: SmellReport
 
@@ -17,6 +17,10 @@ class ConceptC(object):
     def __init__(self, smellReport: SmellReport):
         self.smellReport = smellReport
 
-    def begin(self):
+    def run(self):
+        return None
+
+    def averageSmellValuePerDay(self) -> pd.DataFrame:
+        
         return None
     
