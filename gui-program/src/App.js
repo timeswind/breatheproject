e0b2ac12f5fd2ff371a068e9372c17e799d18e50
Mingtian Yang <mky5089@psu.edu>
2019-11-06 18:37:16 -0500

    initialize GUI

--- gui-program/src/App.js
+++ gui-program/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { PythonShell } from 'python-shell';
 
 function App() {
+
+  function test() {
+    PythonShell.run('../hello.py', null, function (err, results) {
+      if (err) throw err;
+      console.log('hello.py finished.');
+      console.log('results', results);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +27,15 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+
+        <div>
+          Start Date:
+    <input type="date" name="start_date" />
+          End Date:
+    <input type="date" name="end_date" />
+        </div>
+        <button onClick={test}></button>
       </header>
     </div>
   );
