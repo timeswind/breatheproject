5046676e6a1a296bbbe09516d8053850625ebde9
Mingtian Yang <mky5089@psu.edu>
2019-11-07 00:47:17 -0500

    update

--- gui-program/src/App.js
+++ gui-program/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { DatePicker, Button, Select } from 'antd';
 import './App.css';
+import fs from 'fs'
 import 'antd/dist/antd.css';
 import { PythonShell } from 'python-shell';
-import { throws } from 'assert';
 import process from 'process';
 import path from 'path';
 import moment from 'moment';
+import { CsvToHtmlTable } from 'react-csv-to-table';
+
 const nativeImage = require('electron').nativeImage
 
 const { Option } = Select;
-const { MonthPicker, RangePicker } = DatePicker;
+const { RangePicker } = DatePicker;
 const dateFormat = 'YYYY/MM/DD';
-const monthFormat = 'YYYY/MM';
-const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY'];
 
 let pathname = process.cwd()
 let workingPath = path.dirname(pathname);
@@ -23,7 +23,15 @@ PythonShell.defaultOptions = { scriptPath: workingPath, cwd: workingPath };
 function ResultImage({ imagePath }) {
   let image = nativeImage.createFromPath(imagePath)
   return (
-    <img src={image.toDataURL()} className="result-plot-image" />
+    <img src={image.toDataURL()} alt="results" className="result-plot-image" />
+  );
+}
+
+
+function ResultCSV({ csvPath }) {
+  var contents = fs.readFileSync(csvPath, 'utf8');
+  return (
+    <CsvToHtmlTable data={contents} csvDelimiter="," tableClassName="table table-striped table-hover"/>
   );
 }
 
@@ -32,17 +40,18 @@ class App extends React.Component {
   state = {
     status: "Wait for input",
     choosedAnalysisFunction: "select",
-    displayImagePath: null
+    displayImagePath: null,
+    displayCsvPath: null
   }
 
   // '/Users/mingtianyang/Documents/Penn State/2019fall/EDSGN460/python program/results/Allegheny_daily_pm25_mean_binned.png'
 
   analysisFunctions = {
-    "smellReport": "SmellReport.py",
     "BreatheMeter": "BreatheMeter.py",
     "smellPGHStatistics": "SmellPGHStatistics.py",
     "EJAAnalysis": "EJAAnalysis.py",
-    "AppUsage": "AppUsage.py"
+    "AppUsage": "AppUsage.py",
+    "SmellValueAndPM25": "SmellValueAndPM25.py"
   }
 
   selectFunctionOnChange = (value) => {
@@ -50,19 +59,28 @@ class App extends React.Component {
     this.setState({ choosedAnalysisFunction: value });
   }
 
-  test() {
+  analyse() {
     let self = this
     let selectedFunction = this.state.choosedAnalysisFunction
     let filePath = this.analysisFunctions[selectedFunction]
     console.log(filePath)
-    this.setState({status: "Analysing...", displayImagePath: null})
+    this.setState({ status: "Analysing...", displayImagePath: null, displayCsvPath: null })
     PythonShell.run(filePath, null, function (err, results) {
       if (err) throw err;
       let resultFilePath = results.pop();
-      let imageFilePath = path.join(workingPath, resultFilePath)
-      self.setState({ displayImagePath: imageFilePath, status: "Result" })
-      console.log('results', resultFilePath);
+      let ext = path.extname(resultFilePath);
+      let fullFilePath = path.join(workingPath, resultFilePath)
+      if (ext === '.csv') {
+        self.setState({ displayCsvPath: fullFilePath, status: "Dataset Result" })
+      } else {
+        self.setState({ displayImagePath: fullFilePath, status: "Image Result" })
+        console.log('results', resultFilePath);
+      }
     });
+  }
+
+  clear() {
+    this.setState({displayCsvPath: null, displayImagePath: null})
   }
 
   imagePathToDataURL = function (imagePath) {
@@ -75,6 +93,11 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
+          <div className="logo_header">
+            <img src={'/logo_BreatheProject3-1024x483.png'} id="breatheproject-logo" alt="breatheproject logo" />
+            <img src={'/PS_HOR_RGB_2C.png'} id="pennstate-logo" alt="pennstate logo" />
+
+          </div>
           {/* <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -87,36 +110,41 @@ class App extends React.Component {
           >
             Learn React
           </a> */}
-          <div className="element-container">
-            <RangePicker
-              defaultValue={[moment('2016/06/01', dateFormat), moment('2018/12/31', dateFormat)]}
-              format={dateFormat}
-            />
-          </div>
-          <div className="element-container">
+          <div className="form_section">
+            <div className="element-container">
+              <RangePicker
+                defaultValue={[moment('2016/06/01', dateFormat), moment('2018/12/31', dateFormat)]}
+                format={dateFormat}
+              />
+            </div>
+            <div className="element-container">
 
-            <Select
-              showSearch
-              style={{ width: 200 }}
-              placeholder="Please choose an analysis function"
-              optionFilterProp="children"
-              onChange={this.selectFunctionOnChange}
-              filterOption={(input, option) =>
-                option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-              }
-            >
-              <Option value="smellReport">smellReport</Option>
+              <Select
+                showSearch
+                style={{ width: 400 }}
+                placeholder="Please choose an analysis function"
+                optionFilterProp="children"
+                onChange={this.selectFunctionOnChange}
+                filterOption={(input, option) =>
+                  option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                }
+              >
+                <Option value="SmellValueAndPM25">SmellValueAndPM25</Option>
+                <Option value="smellPGHStatistics">smellPGHStatistics</Option>
+                <Option value="EJAAnalysis">EJAAnalysis</Option>
+                <Option value="BreatheMeter">BreatheMeter</Option>
+                <Option value="AppUsage">AppUsage</Option>
+              </Select>
+            </div>
 
-              <Option value="smellPGHStatistics">smellPGHStatistics</Option>
-              <Option value="EJAAnalysis">EJAAnalysis</Option>
-              <Option value="BreatheMeter">BreatheMeter</Option>
-              <Option value="AppUsage">AppUsage</Option>
-            </Select>
+            <div className="element-container">
+              <Button type="primary" onClick={() => this.analyse()}>Analyse</Button>
+              <Button onClick={() => this.clear()} style={{marginLeft: "8px"}}>Clear</Button>
+
+            </div>
           </div>
 
-          <div className="element-container">
-            <Button type="primary" onClick={() => this.test()}>Analyse</Button>
-          </div>
+
 
         </header>
         <div>
@@ -126,6 +154,10 @@ class App extends React.Component {
           <h2>{this.state.status}</h2>
           {this.state.displayImagePath !== null && (
             <ResultImage imagePath={this.state.displayImagePath} />
+          )}
+
+          {this.state.displayCsvPath !== null && (
+            <ResultCSV csvPath={this.state.displayCsvPath} />
           )}
         </div>
       </div>
